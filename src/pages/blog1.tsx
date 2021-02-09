2c50288721b4a8de022406197ba541cea8cb0465
Ubaid-Ali <syedubaidali2020@gmail.com>
2021-02-10 03:02:43 +0500

    create page and add Link

---
+++ src/pages/blog1.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const Blog1 = () => {
+  return (
+    <div>
+      <h1>1st Blog</h1>
+      <Link to="/">Home</Link>
+      <p>Hello guys this is my first blog</p>
+      <p>Hello guys this is my first blog</p>
+    </div>
+  );
+};
+
+export default Blog1;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default () => {
+  return (
+    <div>
+      <h1> index file</h1>
+      <Link to="/blog1">Blog1</Link>
+    </div>
+  );
+};
